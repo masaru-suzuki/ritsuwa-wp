838a99818d174c8c8a2b0c55e9ed98b7f5984960
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-02-17 11:07:57 +0900

    set permlink in news-section ( front-page)

--- front-page.php
+++ front-page.php
@@ -112,28 +112,17 @@ get_header();
             if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
                 // タイトル
                 $title = wp_trim_words(get_the_title(), 10, '...');
-
-                // 記事のカテゴリー情報を取得する
-                $cat = get_the_category();
-                $cat_name = $cat[0]->cat_name; // カテゴリー名
-
                 //記事のテキスト
                 $content = wp_trim_words(get_the_content(), 14, '...');
-
-                //記事のタグを取得(1つ)
-                $tag = get_the_tags();
-                $tag_name = $tag[0]->name;
-
                 //記事の作成日
                 $date = get_the_date('Y-n-j');
-
-                //施設名
-                $facility = get_the_author();
+                //記事のリンク
+                $link = get_permalink();
             ?>
             <tr>
-              <td class="table-txt ymd"><?php echo $date; ?></td>
-              <td class="table-txt"><?php echo $title; ?></td>
-              <td class="table-txt"><?php echo $content; ?></td>
+              <td class="table-txt ymd"><a href="<?php echo $link; ?>"></a><?php echo $date; ?></td>
+              <td class="table-txt"><a href="<?php echo $link; ?>"></a><?php echo $title; ?></td>
+              <td class="table-txt"><a href="<?php echo $link; ?>"></a><?php echo $content; ?></td>
             </tr>
 
             <?php endwhile;
