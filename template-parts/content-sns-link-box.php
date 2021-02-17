f5532c7d2c45d1718f191434692e2c9d6e7bcb2e
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-02-17 12:02:50 +0900

    fix css (global nav)

--- template-parts/content-sns-link-box.php
+++ template-parts/content-sns-link-box.php
@@ -11,14 +11,14 @@
 ?>
 
 <?php if (is_page() || is_front_page()) : ?>
-  <div class="sns-link main-visual__links sp-none">
-    <a href="<?php echo get_page_link(2126); ?>"><i class="fab fa-facebook fa-2x"></i></a>
-    <a href="<?php echo get_page_link(2126); ?>"><i class="fab fa-instagram fa-2x"></i></a>
-  </div>
+<div class="sns-link main-visual__links sp-none">
+  <a href="https://www.instagram.com/ritsuwa_pr/"><i class="fab fa-facebook fa-2x"></i></a>
+  <a href="https://www.instagram.com/ritsuwa_pr/"><i class="fab fa-instagram fa-2x"></i></a>
+</div>
 <?php else : //footerはこっち
 ?>
-  <div class="sns-link">
-    <a href="<?php echo get_page_link(2126); ?>"><i class="fab fa-facebook fa-2x"></i></a>
-    <a href="<?php echo get_page_link(2126); ?>"><i class="fab fa-instagram fa-2x"></i></a>
-  </div>
+<div class="sns-link">
+  <a href="https://www.instagram.com/ritsuwa_pr/"><i class="fab fa-facebook fa-2x"></i></a>
+  <a href="https://www.instagram.com/ritsuwa_pr/"><i class="fab fa-instagram fa-2x"></i></a>
+</div>
 <?php endif; ?>
